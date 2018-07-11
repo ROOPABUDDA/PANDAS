b22e9fdb32beddc048df3ab5a5478e487b887ab0
ROOPA <roopabudda@gmail.com>
2018-07-11 19:08:15 +0530

    dataframe with numpy array
    Creating a DataFrame by passing a NumPy array, with a datetime index and labeled columns

--- dataframe_with_numpyarray.py
+++ dataframe_with_numpyarray.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
 
 dates = pd.date_range('20180711',periods=6)
 print(dates)
+
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
+print(df)
