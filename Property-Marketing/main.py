a5f883a44856330ea6a92025daecad6c3b74588e
ROOPA <roopabudda@gmail.com>
2018-08-01 12:17:53 +0530

    [property-dataset]convert class column to numeric

--- Property-Marketing/main.py
+++ Property-Marketing/main.py
@@ -10,3 +10,28 @@ pd.options.display.max_rows
 pd.options.display.max_columns = 24
 df = pd.read_csv("Dataset/Property/Properties.csv")
 print(df)
+
+def class_to_numeric(series):
+    """[summary]:
+    To convert characters to numeric
+
+    [description]
+    this function is used to convert the class values from characters to numeric values.
+
+    Arguments:
+        series {[type]} -- [description]
+        a series of dataframe column 'class'
+
+    Returns:
+        number -- [description]
+    """
+    if series['class'] == 'Class A':
+        return 0
+    elif series['class'] =='Class B':
+        return 1
+    else:
+        return 2
+
+
+df['class'] = df.apply(class_to_numeric, axis='columns')
+print(df['class'])
