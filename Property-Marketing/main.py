f4c6d71a2471634eff43a1087b1c5feda4a8dabb
ROOPA <roopabudda@gmail.com>
2018-08-01 10:45:16 +0530

    [property-marketing]read property dataset

---
+++ Property-Marketing/main.py
@@ -0,0 +1,12 @@
+# version 0.0.0
+# python 3.5
+# author ROOPA BT 31/07/2018
+
+
+import numpy as np
+import pandas as pd
+# from . import Dataset
+pd.options.display.max_rows
+pd.options.display.max_columns = 24
+df = pd.read_csv("Dataset/Property/Properties.csv")
+print(df)
